e937982e036391c9102f0fc48d3a7ca9bc25ffb5
asoolin <asoolin1021@gmail.com>
2022-12-22 21:57:20 +0100

    Update files

--- LeagueLanguageChanger.py
+++ LeagueLanguageChanger.py
@@ -1,69 +1,96 @@
 from tkinter import *
 import tkinter as tk
+import subprocess
 
-#scroller
+language_map = {
+    "Japanese": "ja_JP",
+    "Korean": "ko_KR",
+    "English": "en_GB",
+    "Chinese (Traditional)": "zh_TW",
+    "Chinese (Simplified)": "zh_CN",
+    "Czech": "cz_CZ",
+    "German": "de_DE",
+    "Greek": "el_GR",
+    "Spanish": "es_ES",
+    "French": "fr_FR",
+    "Hungarian": "hu_HU",
+    "Italian": "it_IT",
+    "Polish": "pl_PL",
+    "Portuguese": "pt_BR",
+    "Romanian": "ro_RO",
+    "Russian": "ru_RU",
+    "Turkish": "tr_TR",
+}
+
+my_list = ['Chinese (Traditional)', 
+        'Chinese (Simplified)', 
+        'Czech', 
+        'English', 
+        'French', 
+        'German', 
+        'Greek', 
+        'Hungarian', 
+        'Italian', 
+        'Japanese', 
+        'Korean', 
+        'Polish', 
+        'Portuguese', 
+        'Romanian', 
+        'Russian', 
+        'Spanish', 
+        'Turkish']
+
+def quit():
+    window.quit()
+
+def dal():
+    active_language = l1.get('active')
+    language = language_map.get(active_language)
+
+    with open("C:\Riot Games\League of Legends\system.yaml", "r") as f:
+        lines = f.readlines()
+
+    lines[167] = f"    - {language}\n"
+    lines[172] = f"    default_locale: {language}\n"
+
+    with open("C:\Riot Games\League of Legends\system.yaml", "w") as f:
+        f.writelines(lines)
+
+    with open("C:\Riot Games\League of Legends\Config\LeagueClientSettings.yaml", "r") as f:
+        lines = f.readlines()
+
+        lines[12] = f"        locale: {language}\n"
+
+    with open("C:\Riot Games\League of Legends\Config\LeagueClientSettings.yaml", "w") as f:
+        f.writelines(lines)
+
+    subprocess.run(
+        r"C:\Riot Games\League of Legends\LeagueClient.exe", check=True
+    )
+
 window = tk.Tk()
 window.title("Language Selection")
+window.resizable(False,False)
 window.geometry("300x200")
-
 window.rowconfigure([0, 1], minsize=5, weight=1)
 window.columnconfigure([0, 1], minsize=50, weight=1)
 
-l1=tk.Label(window,text='Language List')
+l1=tk.Label(window,text='Language List "Scrollable"')
 l1.grid(row=0,column=0,columnspan=1,pady=0)
 
-def close():
-    window.quit()
+vscroll=tk.Scrollbar(window, orient=VERTICAL)
+vscroll.grid(row=1, column=2, sticky=N+S)
 
-def swap():
-    if l1.get('active') == "Japanese":
-        language = "ja_JP"
-    elif l1.get('active') == "Korean":
-        language = "ko_KR"
-    elif l1.get('active') == "English":
-        language = "en_GB"
-    elif l1.get('active') == "Chinese (Traditional)":
-        language = "zh_TW"
-    elif l1.get('active') == "Chinese (Simplified)":
-        language = "zh_CN"
-        
-    def Run():
-        line_num1 = 167
-        line_num2 = 172
-        line_num3 = 12
-
-        def replace_line(file_name, line_num, text):
-            lines = open(file_name, 'r').readlines()
-            lines[line_num] = text
-            out = open(file_name, 'w')
-            out.writelines(lines)
-            out.close()
-        replace_line("C:\Riot Games\League of Legends\system.yaml", line_num1, '    - {}\n'.format(language))
-        replace_line("C:\Riot Games\League of Legends\system.yaml", line_num2, '    default_locale: {}\n'.format(language))
-            
-        def replace_line2(file_name, line_num, text):
-            lines = open(file_name, 'r').readlines()
-            lines[line_num] = text
-            out = open(file_name, 'w')
-            out.writelines(lines)
-            out.close()
-        replace_line2("C:\Riot Games\League of Legends\Config\LeagueClientSettings.yaml", line_num3, '        locale: {}\n'.format(language))
-    
-        import sys, string, os
-        os.popen(r"C:\Riot Games\League of Legends\LeagueClient.exe")
-
-    Run()
-
-selectbutton=Button(window, text="Select", command=lambda:[swap(),close()])
-selectbutton.grid(row=1, column=1)
-
-my_list=["Japanese", "Korean", "English", "Chinese (Traditional)", "Chinese (Simplified)"]
-l1 = tk.Listbox(window,height=5) #, yscrollcommand = sb.set)
+l1 = tk.Listbox(window,height=7, yscrollcommand=vscroll.set)
 l1.grid(row=1,column=0,padx=20,pady=20) 
+
+Scrollbar
+vscroll.config(command=l1.yview)
 
 for element in my_list: # adding elements to Listbox
     l1.insert(tk.END,element)
 
-#sb.config( command = l1.yview )
+selectbutton=Button(window, text="Select", command=lambda:[dal(),quit()])
+selectbutton.grid(row=1, column=1)
 
 mainloop()
